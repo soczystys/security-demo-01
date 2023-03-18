04fe1ea0b6b8e925a713d3af4da6de472262d44d
kmalinowski957 <kmalinowski957@gmail.com>
2023-03-18 21:04:38 +0100

    Add starter files

---
+++ src/main/java/com/demos/securitydemo01/SecurityDemo01Application.java
@@ -0,0 +1,13 @@
+package com.demos.securitydemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDemo01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demos/securitydemo01/SecurityDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demos.securitydemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
